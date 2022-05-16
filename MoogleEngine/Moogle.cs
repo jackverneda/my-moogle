0b25aa5b8cc8dd0d5cc570957add61a2002dc404
jacksonvera <jacksonverapineda@icloud.com>
2022-05-16 15:26:37 -0400

    the-really-first-commit

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -6,9 +6,11 @@ public static class Moogle
     public static SearchResult Query(string query) {
         // Modifique este método para responder a la búsqueda
 
-        SearchItem[] items = new SearchItem[3] {
+        SearchItem[] items = new SearchItem[5] {
             new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
             new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
+            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
             new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
         };
 
